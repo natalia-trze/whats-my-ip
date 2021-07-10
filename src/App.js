722f9825f9b29b7ad24cd64256ad573411263ea1
Natalia Trzeciak <trzeciak.natalia@icloud.com>
2021-07-10 17:09:05 +0200

    second-fetch

--- src/App.js
+++ src/App.js
@@ -2,32 +2,46 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import MyNavbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import MyMap from "./MyMap"
-const {REACT_APP_API_KEY} = process.env;
+import CarouselComponent from './Carousel';
+const { REACT_APP_API_KEY } = process.env;
 
 export default function App() {
-  const [userData, setUserData] = useState({});  
+  const [userData, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(true)
+  const [countryInfo, setCountryInfo] = useState({})
 
-  
-  useEffect(()=> {
-      fetch(`https://geo.ipify.org/api/v1?apiKey=${REACT_APP_API_KEY}&ipAddress`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log( data)
-          setUserData(data)
-        });
-  }, [])
+  useEffect(() => {
+
+    fetch(`https://geo.ipify.org/api/v1?apiKey=${REACT_APP_API_KEY}&ipAddress`, {
+      method: 'get'
+    })
+      .then((res) => res.json())
+      .then(async (data) => {
+        setUserData(data)
+        const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${data.location.country}`);
+        const result_1 = await res.json();
+        setCountryInfo(result_1);
+        setIsLoading(false);
+  })
+}, [])
 
 
   return (
     <div className="App">
       <MyNavbar />
-      <h3>your IP is: {userData.ip}</h3>
-      
-      <MyMap/>
+      {isLoading ?
+        (<h1>LOADING ...</h1>)
+        :
+        (
+          <div>
+            <h3>{countryInfo.name}</h3>
+            <h3>your IP is: {userData.ip}</h3>
+            <CarouselComponent fetchedData={userData} countryInfo={countryInfo}/>
+          </div>
+        )}
+
     </div>
   );
 }
 
-
+  
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,40 +1,39 @@
 import { Carousel } from "react-bootstrap";
+import MyMap from "./MyMap";
 
-
-const CarouselComponent = ({userdata}) => {
+const CarouselComponent = ({fetchedData, countryInfo}) => {
   return (
     <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://images.unsplash.com/photo-1514810771018-276192729582?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="First slide"
-        />
-        <Carousel.Caption className="d-block1">
-
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1514810771018-276192729582?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt={userdata.ip}
+          src="https://cdn.crispedge.com/43464b.png"
+          alt="Card "
         />
         <Carousel.Caption>
-          <h3> {userdata.ip}</h3>
+          <MyMap geoLocation={fetchedData} />
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://images.unsplash.com/photo-1514810771018-276192729582?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          src="https://cdn.crispedge.com/43464b.png"
+          alt="second slide"
+        />
+        <Carousel.Caption>
+          <h3> {fetchedData.ip}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.crispedge.com/43464b.png"
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <img src={countryInfo.flag} />
+          <h3>Country: {countryInfo.name}</h3>
+          <h3>Numeric Code: {countryInfo.numericCode} </h3>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
